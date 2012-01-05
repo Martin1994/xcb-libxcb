0e9246def562be97cc816f824f702233a826ec56
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-05 20:57:53 +0000

    Fix include order with Xdmcp on WIN32
    Fix a redefinition problem due to include order which shows up when
    building for _WIN32 and libXdmcp is installed, so HASXDMAUTH is enabled
    
    Signed-off-by: Jon TURNEY <jon.turney@dronecode.org.uk>
    Reviewed-by: Peter Harris <pharris@opentext.com>
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- src/xcb_auth.c
+++ src/xcb_auth.c
@@ -36,6 +36,10 @@
 #include <rpc/types.h>
 #endif
 
+#ifdef HASXDMAUTH
+#include <X11/Xdmcp.h>
+#endif
+
 #ifdef _WIN32
 #include "xcb_windefs.h"
 #else
@@ -48,9 +52,6 @@
 #include "xcb.h"
 #include "xcbint.h"
 
-#ifdef HASXDMAUTH
-#include <X11/Xdmcp.h>
-#endif
 
 enum auth_protos {
 #ifdef HASXDMAUTH
